98286a95d99f4757cef6e3648d6df2d4af2cf00b
Ferran <ferran.docencia@gmail.com>
2026-04-24 20:05:15 +0200

    JS : dia 1

---
+++ 01_variables.js
@@ -0,0 +1,42 @@
+// var es la forma antigua de crear una variable
+var variable_1 = 1 // vamos a intentar no usarla
+var variable_1 = 2
+
+console.log (variable_1)
+
+/* let es la forma moderna 
+de crear una variable */
+let variable2 = "hoy es viernes"
+console.log("línea 6", variable2)
+
+variable2 = "mañana será sábado"
+
+const lenguaje = 'Javascript'
+
+// instrucción para mostrar un resultado en la pantalla
+console.log("línea 14", variable2)
+
+
+const PI = 3.1416
+// PI = 7 <-- ERROR: No se puede reasignar el valor de una constante
+
+const IVA18 = 0.18
+
+// Tipo numérico
+let num1 = 20
+let num2 = 20.5
+let num3 = -20
+
+console.log(typeof num1)
+
+let suma = num1 + num2
+let resta = num1 - num2
+let multi = num1 * num2
+let division = num1 / num2
+let potencia = 2**3
+let raizQ = 9**0.5
+console.log(raizQ)
+
+let resto = 20 % 2 // % <-- para obtener el módulo o resto
+console.log(resto)
+
