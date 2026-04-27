e94054f14cef58a3deed86b9e128b88bbf92d5e2
Ferran <ferran.docencia@gmail.com>
2026-04-27 20:18:52 +0200

    2026-04-27

--- 05_if_switch.js
+++ 05_if_switch.js
@@ -26,3 +26,43 @@ if (horaActual >= 7 && horaActual <= 14) {
 } else {
   console.log("Buenas noches");
 }
+
+// SWITCH
+// switch (valor) {
+//   case "1":
+//     ...
+//     break;
+//   case 2:
+//     ...
+//     break;
+//   default:
+//     ...
+// }
+
+let diaEnNumero = 3
+// Ejemplo:
+switch (diaEnNumero) {
+  case "27":
+    alert("Hoy es lunes 27 de abril");
+    break;
+  case "28":
+    alert("Hoy es martes 28 de abril");
+    break;
+  case "29":
+    alert("Hoy es miércoles 29 de abril");
+    break;
+  case "30":
+    alert("Hoy es jueves 30 de abril");
+    break;
+  case "1":
+    alert("Hoy es viernes 1 de mayo");
+    break;
+  case "2":
+    alert("Hoy es sábado 2 de mayo");
+    break;
+  case "3":
+    alert("Hoy es domingo 3 de mayo");
+    break;
+  default:
+    alert("Valor no aceptado");
+}
