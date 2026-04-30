9b3406835b197b1fcc83344446b2dd7fa6650e69
Ferran <ferran.docencia@gmail.com>
2026-04-30 18:10:47 +0200

    2026-04-30

---
+++ 17_ejer_08_censurador.js
@@ -0,0 +1,14 @@
+// CENSURADOR
+
+/*
+Crea una función que reciba un texto.
+En ese texto va a "censurar" mediante un asterisco por cada
+letra una serie de palabras previamente establecidas en un
+array (y que puede incrementarse):
+tonto, tonta, idiota, etc.
+
+Por ejemplo, si el texto inicial fuera:
+Esa persona es tonta. Incluso idiota.
+Debería devolver:
+Esa persona es *****. Incluso ******.
+*/
---
+++ 18_ejer_09_contador de caracteres.js
@@ -0,0 +1,15 @@
+// CONTADOR DE CARACTERES
+
+/*
+Crea una función que cuente los caracteres de un texto.
+Solo las letras, no los espacios, comas, puntos, etc.
+
+Por ejemplo, en el texto:
+Mañana es viernes
+Debería devolver:
+Hay 1 m, 3 a, 1 ñ, 2 n, 3 e, 2 s, 1 v, 1 i, 1 r.
+
+*/
+
+
+
